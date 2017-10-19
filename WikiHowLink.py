2296c7476a0f66ef6a3597af09de6bee8d8f2cd1
riccardorestagno <riccardo.restagno@mail.mcgill.ca>
2017-10-19 15:55:21 -0400

    Update WikiHowLink.py

--- WikiHowLink.py
+++ WikiHowLink.py
@@ -1,8 +1,19 @@
 import praw
 import time
+from datetime import datetime, timedelta
+
+
+def minutes_posted(submission):
+	"""Gets the time that passed (in minutes) from when the post was made. (All time is converted to UTC)"""
+	time_created = submission.created_utc
+	current_time = datetime.utcnow()
+	time_posted = datetime.utcfromtimestamp(time_created)
+	time_difference_in_minutes = (current_time - time_posted)/timedelta(minutes=1)
+	return time_difference_in_minutes
 
 def reddit_bot(link, title, reminder):
-	
+	"""If post was made longer than 10 minutes ago, module checks if wikihow link is a top-level comment
+If true, post is skipped. If false, comment is made on post, then it is deleted"""
 	reddit = praw.Reddit(client_id='',
 					client_secret= '',
 					user_agent='',
@@ -17,6 +28,7 @@ def reddit_bot(link, title, reminder):
 	# For more info, go to (http://praw.readthedocs.io/en/latest/tutorials/comments.html) 
 	submission.comments.replace_more(limit=0)
 	
+	#searches through top-level comments and checks if there is a wikihow link in them
 	for top_level_comment in submission.comments:
 		if 'wikihow.com/' in top_level_comment.body:
 			wikihowlink = True
@@ -24,18 +36,27 @@ def reddit_bot(link, title, reminder):
 	if wikihowlink == False:
 		print(title)
 		submission.reply(reminder) #replys to post
+		submission.mod.remove() #deletes the post
 		time.sleep(5) # Prevents praw from detecting spam
 
 if __name__ == "__main__":
-	post_link_reminder_text = 'A friendly reminder to comment on this post the wikihow link from where this picture is from' 
+	post_link_reminder_text = """Hello user. Thank you for your submission. However, it has been removed for the following reason(s):  
+
+Rule 2: All posts must provide the source WikiHow article as a link in the comments.
+
+
+Please add a comment linking to the source article, then [message the mods](http://www.reddit.com/message/compose?to=%2Fr%2FDisneyVacation) and provide us with the link to the comment's section of your post."""
 
 	reddit = praw.Reddit(client_id='',
 					client_secret= '',
 					user_agent='')
 
-	subreddit = reddit.subreddit('reddit_bot_tester')
-	submissions = subreddit.new(limit=40)
+	subreddit = reddit.subreddit('disneyvacation')
+	submissions = subreddit.new(limit=20)
 
-	#gets url of newest posts on disneyvacations
+	#gets url of newest posts on disneyvacation
 	for submission in submissions:
+		#Goes to next loop iteration if post was made less than 10 minutes ago
+		if minutes_posted(submission) < 10:
+			continue
 		reddit_bot(submission.url, submission.title, post_link_reminder_text)
