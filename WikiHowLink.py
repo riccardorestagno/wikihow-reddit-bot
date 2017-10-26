1c3e3dc594710d7aefbb086db4195a7c37865e52
Matthew Wolffsohn <mwolfie@protonmail.com>
2017-10-26 19:10:29 +0100

    Revert "Merge branch 'master' of https://github.com/riccardorestagno/WIKIHow-Reddit-Bot"
    This reverts commit 571c1159d001135750adaa6e39042f7f7ff4061e, reversing
    changes made to 9131349b0d61a2fa1b6d197e5bafa95680dc71e3.

--- WikiHowLink.py
+++ WikiHowLink.py
@@ -20,9 +20,8 @@ If true, post is skipped. If false, comment is made on post, then it is deleted"
 					username='',
 					password='')
 					
-	
-	wikihow_domains = [ 'wikihow.com/','wikihow.mom/']	# Different possible wikihow domains
-	submission = reddit.submission(url = 'https://www.reddit.com' + link)
+					
+	submission = reddit.submission(url = link)
 	wikihowlink = False
 
 	# Prevents exception (AttributeError: 'MoreComments' object has no attribute 'body')
@@ -31,7 +30,7 @@ If true, post is skipped. If false, comment is made on post, then it is deleted"
 	
 	#searches through top-level comments and checks if there is a wikihow link in them
 	for top_level_comment in submission.comments:
-		if any(urls in top_level_comment.body for urls in wikihow_domains): # Checks if any wikihow domains are linked in the comments
+		if 'wikihow.com/' in top_level_comment.body:
 			wikihowlink = True
 			
 	if wikihowlink == False:
@@ -60,4 +59,4 @@ Please add a comment linking to the source article, then [message the mods](http
 		#Goes to next loop iteration if post was made less than 10 minutes ago
 		if minutes_posted(submission) < 10:
 			continue
-		reddit_bot(submission.permalink, submission.title, post_link_reminder_text)
+		reddit_bot(submission.url, submission.title, post_link_reminder_text)
