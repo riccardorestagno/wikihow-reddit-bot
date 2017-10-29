df0c9cfbb39ddc769fe91fd455eacbee802d60ec
riccardorestagno <riccardo.restagno@mail.mcgill.ca>
2017-10-29 19:51:26 -0400

    Update WikiHowLink.py

--- WikiHowLink.py
+++ WikiHowLink.py
@@ -11,7 +11,7 @@ def minutes_posted(submission):
 	time_difference_in_minutes = (current_time - time_posted)/timedelta(minutes=1)
 	return time_difference_in_minutes
 
-def sticky_and_delete(link):
+def sticky_and_delete(link, filepath):
 	""" Post where wikihow bot made a comment in is called and the comment is stickied and the post is deleted"""
 	reddit = praw.Reddit(client_id='',
 		client_secret= '',
@@ -26,11 +26,15 @@ def sticky_and_delete(link):
 	for top_level_comment in submission.comments:
 		if top_level_comment.author == 'WikiHowLinkBot':
 			top_level_comment.mod.distinguish(how='yes', sticky=True)
+			with open(filepath, 'a') as outputfile:
+				outputfile.writelines(", the post has been stickied and distinguished")
 			break
 		
 	submission.mod.remove() #deletes the post
+	with open(filepath, 'a') as outputfile:
+		outputfile.writelines(", and has been deleted!!\n")
 	
-def comment_on_post(link, title, reminder):
+def comment_on_post(link, title, reminder, filepath):
 	"""If post was made longer than 10 minutes ago, module checks if wikihow link is a top-level comment
 If true, post is skipped. If false, comment is made on post, then another definition is called to sticky and delete post"""
 	reddit = praw.Reddit(client_id='',
@@ -59,12 +63,15 @@ If true, post is skipped. If false, comment is made on post, then another defini
 		webbrowser.open_new_tab('https://www.reddit.com' + link)
 		submission.reply(reminder) #replys to post
 		print("Reply done")
+		with open(filepath, 'a') as outputfile:
+			outputfile.writelines("Title of post searched: " + title + " - Post didn not include wikihow link in comments. The bot successfully replied to the post")
 		time.sleep(7) # Prevents praw from detecting spam and also gives enough time for reply to register before calling sticky_and_delete
-		sticky_and_delete(link)	
+		sticky_and_delete(link, filepath)	
 		print("Sticky done")
 		#time.sleep(60) # Gives Xalaxis time to check the bot is working	
 		
 if __name__ == "__main__":
+	filepath = r"C:\Users\......\WikiHowBotLog.txt"
 	post_link_reminder_text = """Hello user. Thank you for your submission. However, it has been removed for the following reason(s):  
 
 Rule 2: All posts must provide the source WikiHow article as a link in the comments.
@@ -84,4 +91,4 @@ Please add a comment linking to the source article, then [message the mods](http
 		#Goes to next loop iteration if post was made less than 10 minutes ago
 		if minutes_posted(submission) < 10:
 			continue
-		comment_on_post(submission.permalink, submission.title, post_link_reminder_text)
+		comment_on_post(submission.permalink, submission.title, post_link_reminder_text, filepath)
