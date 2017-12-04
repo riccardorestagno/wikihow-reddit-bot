e5504153e3560a43f262aff8425db814d776f6c1
Rick Restagno <riccardo.restagno@mail.mcgill.ca>
2017-12-03 19:03:04 -0500

    Removes %3f (equivalent to '?')

--- WikiHowLink.py
+++ WikiHowLink.py
@@ -20,7 +20,8 @@ def mobile_to_desktop_link(mobile_link, post_reapproval):
 	desktop_link = desktop_link.rsplit('m.wikihow.', 1)[1] #removes hyperlinks
 	desktop_link = 'https://www.wikihow.' + desktop_link.rsplit('?', 1)[0] #removes redirects
 	desktop_link = desktop_link.rsplit('amp=', 1)[0] # removes 'amp' mobile tag
-	#desktop_link = desktop_link.split('%', 1)[0]
+	desktop_link = desktop_link.split('%3F', 1)[0]
+	desktop_link = desktop_link.split('%3f', 1)[0]
 		
 	if post_reapproval == True:	
 		return 'Source: ' + desktop_link
