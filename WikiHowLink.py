9cf85c709c869d3406eb9ca8804e864de3585b58
Rick Restagno <riccardo.restagno@mail.mcgill.ca>
2018-03-16 16:29:17 -0400

    Mod posts no longer get auto-deleted if not flaired

--- WikiHowLink.py
+++ WikiHowLink.py
@@ -98,7 +98,7 @@ If true, post is skipped. If false, comment is made on post, then another defini
 
 	#Checks if post has meta tag
 	try:
-		if submission.link_flair_text.lower() == 'meta' and submission.domain.startswith('self.'):
+		if (submission.link_flair_text.lower() == 'meta' and submission.domain.startswith('self.')) or submission.author.name in disneyvacation_mods::
 			wikihowlink = True
 			with open(filepath, 'a') as outputfile:
 				outputfile.writelines("Post PASSED - " + title + " (Meta Tag)" + "\n")
@@ -111,7 +111,7 @@ If true, post is skipped. If false, comment is made on post, then another defini
 		#searches through top-level comments and checks if there is a wikihow link in them
 		for top_level_comment in submission.comments:
 			# Checks if any wikihow domains are linked in the comments or if mods already replied to post
-			if any(urls in top_level_comment.body for urls in wikihow_domains) or any(mods == top_level_comment.author for mods in disneyvacation_mods):
+			if any(urls in top_level_comment.body for urls in wikihow_domains) or any(mods == top_level_comment.author.name for mods in disneyvacation_mods):
 				wikihowlink = True
 				for comment in top_level_comment.replies:# Checks if bot already replied with a desktop link
 					if comment.author.name == 'WikiHowLinkBot':
