1d84475991c26e7b29a07b9fed204e9fbed251f8
Riccardo Restagno <riccardo.restagno@gmail.com>
2021-03-12 20:24:36 -0500

    Handle HTTP error code 502

--- wikihowbot/wikihowbot.py
+++ wikihowbot/wikihowbot.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
             print("Sweep finished @ " + str(datetime.now()))
             time.sleep(5 * 60)  # Wait for 5 minutes before running again.
         except prawcore.exceptions.ResponseException as httpError:
-            if httpError.response.status_code == 503:
+            if httpError.response.status_code == 503 or httpError.response.status_code == 502:
                 log_message(f"Reddit is temporarily down. Waiting 5 minutes.")
                 time.sleep(5 * 60)  # Temporary connection error. Wait for 5 minutes before running again.
             else:
